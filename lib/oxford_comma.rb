085a500228e5a1e7e9d6e147ec03c26d36f63ebe
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 22:23:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size==1
     return array.join
   elsif array.size==2
-    return array.join("and")
+    return array.join(" and")
   elsif array.size==3
     return array.join(",")
   else
