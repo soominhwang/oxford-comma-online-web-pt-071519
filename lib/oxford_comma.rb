d2846dc9700fe8ae54f079eb2e43c07d4fd16e09
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 22:33:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
   if array.size==1
-    return array.join
+    array.join
   elsif array.size==2
-    return array.join(" and")
+    array.join(" and")
   elsif array.size==3
     array.insert(1,"and")
-    return array.join(",")
+    array.join(",")
   else
     array.insert(-1, "and")
-    return array.join(",")
+    array.join(",")
   end
 end
 
