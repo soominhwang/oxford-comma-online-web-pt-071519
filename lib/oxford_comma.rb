1eddbf0c30c8f366bff75636d51d414c8f02ea97
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 22:22:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-  return array.join
+  if array.size==1
+    return array.join
+  elsif array.size==2
+    return array.join("and")
+  elsif array.size==3
+    adds comma plus a final and
+  else
+    correctly formats arrays of lengths
+  end
+end
+
+
+
+
+
 end
